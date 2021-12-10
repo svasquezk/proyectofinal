8d34e94afc882008eff41e976c6f0256535047cf
svasquezk <savasquezk@gmail.com>
2021-12-09 22:04:34 -0300

    Entrega parcial, avance con loguin

--- src/app/ingreso/registro/registro.component.ts
+++ src/app/ingreso/registro/registro.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Cliente } from '../cliente.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  @Output() clienteAdd : EventEmitter<any> = new EventEmitter();
+  
+  frmRegistro = new FormGroup({
+    nomUsuario: new FormControl('', Validators.required),
+    correo: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  cliente: Cliente[] = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  crearRegistro() {
+    console.log('--->', this.frmRegistro.valid);
+    if(this.frmRegistro.valid) {
+      let cli: Cliente;
+      cli = {
+        nombreUsuario: this.frmRegistro.get('nomUsuario').value,
+        correo: this.frmRegistro.get('correo').value,
+        password: this.frmRegistro.get('password').value
+      }
+      // cli.nombreUsuario = this.frmRegistro.get('nomUsuario').value;
+      // cli.correo = this.frmRegistro.get('correo').value;
+      // cli.password = this.frmRegistro.get('password').value;
+
+      this.cliente.push(cli);
+      this.clienteAdd.emit('dadasjhsiaj');
+      // this.router.navigate(['/login'])
+    }
+
   }
 
 }
